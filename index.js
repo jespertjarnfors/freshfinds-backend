9b4ce1067e559b243401e5630638959a9cd04be2
jespertjarnfors <139288555+jespertjarnfors@users.noreply.github.com>
2024-07-08 17:28:47 +1200

    Added deployd front end to CORS.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Configuring CORS to allow requests from frontend
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://freshfinds-frontend.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
 };
